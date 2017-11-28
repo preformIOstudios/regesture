9f6c7ab2782ca4eb1ebd8fc397c5932f8358ff8c
david allen <d@preform.io>
2017-11-28 14:12:17 -0500

    Log log plot, cleaned up formatting for legibility.

--- create_raster.m
+++ create_raster.m
@@ -6,7 +6,7 @@ function [] = create_raster( fName )
 
 switch nargin
     otherwise
-        fName = "MasterLiuPerformanceChar00.calc";
+        fName = 'MasterLiuPerformanceChar00.calc';
 end 
 
 %import file
@@ -21,63 +21,73 @@ DATA = load(fName);
 %collapse data matrix into 1D array with a single value per frame
 V = sum(DATA, 2);
 %replicate those values into matrix for transposition math
-%TODO: is there an easier way to do this and the next line in one command?
+%TODO: is there an easier (component-wise?) way to do this and the next line in one command?
 VV = repmat(V, 1, numel(V));
 %calculate distance between those vectors into a distance array
 D = abs(VV - VV');
+%calculate scaled log of distances
+d = log(D);
+d = d / max(d(:));
 %display distance matrix (scaled and unscaled)
-figure('NumberTitle', 'off', 'Name', fName + ' Self-Similarity Graph');
-subplot(2,2,1);
-imagesc(D);
-title('Distance Matrix [scaled]');
-subplot(2,2,2);
-image(D);
-title('Distance Matrix');
+figure('NumberTitle', 'off', 'Name', [fName ' Self-Similarity Graph']);
+colormap default %TODO: set image coloration back to default -- why are these images showing up B&W?!?
+    subplot(2,2,1);
+        imagesc(DATA);
+        title('DATA');
+
+    subplot(2,2,2);
+        imshow(d);
+        title('Distance Image [ log, scaled ]');
+        
+    subplot(2,2,3);
+        image(D);
+        title('Distance Matrix');
+        
+    subplot(2,2,4);
+        imagesc(D);
+        title('Distance Matrix [scaled]');
 
 %save out scaled log of distances to an image
-D = log(D);
-D = D / max(D(:));
-subplot(2,2,3);
-imshow(D);
-title('Distance Image [ log, scaled ]');
-%TODO: figure out why this is suddenly not working
-% fName
-% imwrite(D, [fName, '.png']); 
+imwrite(d, [fName, '.png']);
+%TODO: save out figure
+%TODO: move above to its own function and call it here
 
 %---------------
-%TODO: create power / frequency analysis
+%power / frequency analysis
 %reference -- https://www.mathworks.com/help/signal/ug/power-spectral-density-estimates-using-fft.html?requestedDomain=www.mathworks.com
 %---------------
 %HACK: filter out some channels (TODO: should be specified in external data file)
 FFTDATA = DATA(:, 1:337);
 %analyze individual channels / groups of channels
-%plot entire analysis
-figure('NumberTitle', 'off', 'Name', fName + ' FFT analysis');
-subplot(2,2,1);
-imagesc(FFTDATA);
-title('FFTDATA');
+N = size(FFTDATA,1);
+[PRDG, w] = periodogram(FFTDATA,rectwin(N),N,60);
+%plot analysis
+figure('NumberTitle', 'off', 'Name', [fName ' FFT analysis']);
+    subplot(2,2,1);
+        imagesc(FFTDATA);
+        title('FFTDATA');
 
-subplot(2,2,2);
-grid on;
-hold on; % add any enclosed plots to the same graph
-title('fft(FFTDATA)');
-FFTanalysis = fft(FFTDATA);
-x = 0:1/(length(FFTanalysis)-1):1;
-length(x);
-length(FFTanalysis);
-plot(x, FFTanalysis);
+    subplot(2,2,2);
+        grid on;
+        hold on; % add any enclosed plots to the same graph
+            title('priodogram = plot(w, 10*log10(PRDG))');
+            plot(w, 10*log10(PRDG));
+        hold off;
+
+    subplot(2,2,4);
+        grid on;
+        hold on; % add any enclosed plots to the same graph
+            title('log / log plot = plot(log(w), log(PRDG))');
+            plot(log(w), log(PRDG));
+            % TODO: why is this different?
+            %loglog(w, PRDG);
+        hold off;
+        
+    subplot(2,2,3);
+        imagesc(log(PRDG));
+        title('log(PRDG)');
 %composit those into a single p/f graph for entire dataset
 %    - need to consider different methodologies
-hold off;
-subplot(2,2,4);
-grid on;
-N = size(FFTDATA,1);
-N;
-PRDG = periodogram(FFTDATA,rectwin(N),N,60);
-periodogram(FFTDATA,rectwin(N),N,60)
-subplot(2,2,3);
-imagesc(log(PRDG));
-title('PRDG');
 %create a 2D image to represent entire dataset (channels on x, p/f values on y?)
 %save out image(s) / figures(s)
 %---------------
