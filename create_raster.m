2e059821fe32e24e4a3766d296eedef385083b2f
david allen <d@preform.io>
2017-12-02 20:33:38 -0500

    First pass at linear regression calculations.

--- create_raster.m
+++ create_raster.m
@@ -17,6 +17,8 @@ DATA = load(fName);
 %---------------
 %create self-similarity matrix
 %---------------
+figure('NumberTitle', 'off', 'Name', [fName ' Self-Similarity Graph']);
+ColorSet = varycolor(size(DATA, 2));
 %TODO: normalize & filter data rows (channels) 
 %   use only rotation and velocity data
 %   mask out rows with no data
@@ -29,24 +31,25 @@ VV = repmat(V, 1, numel(V));
 %calculate distance between those vectors into a distance array
 D = abs(VV - VV');
 %calculate scaled log of distances
-d = log(D);
-d = d / max(d(:));
+d1 = log(D);
+d = d1 / max(d1(:));
 %display distance matrix (scaled and unscaled)
-figure('NumberTitle', 'off', 'Name', [fName ' Self-Similarity Graph']);
-colormap default %TODO: set image coloration back to default -- why are these images showing up B&W?!?
     subplot(2,2,1);
-        imagesc(DATA);
+        set(gca, 'ColorOrder', ColorSet, 'NextPlot', 'replacechildren');
+        plot(DATA);
         title('DATA');
 
     subplot(2,2,2);
-        imshow(d);
+        imshow(d, 'colormap', gray);
         title('Distance Image [ log, scaled ]');
         
     subplot(2,2,3);
+%         colormap default %TODO: set image coloration back to default -- why are these images showing up B&W?!?
         image(D);
         title('Distance Matrix');
         
     subplot(2,2,4);
+%         colormap default %TODO: set image coloration back to default -- why are these images showing up B&W?!?
         imagesc(D);
         title('Distance Matrix [scaled]');
 
@@ -61,36 +64,71 @@ imwrite(d, [fName, '.png']);
 %power / frequency analysis
 %reference -- https://www.mathworks.com/help/signal/ug/power-spectral-density-estimates-using-fft.html?requestedDomain=www.mathworks.com
 %---------------
+figure('NumberTitle', 'off', 'Name', [fName ' FFT analysis']);
 %HACK: filter out some channels (TODO: should be specified in external data file)
-FFTDATA = DATA(:, 1:337);
+FFTDATA = DATA(:, 1:337); %most elements
+% FFTDATA = DATA(:, 15:21); %7 elements
+% FFTDATA = DATA(:, 15:20); %6 elements
+% FFTDATA = DATA(:, 15:17); %3 elems
+% FFTDATA = DATA(:, 15); %1 elem
+ColorSet = varycolor(size(FFTDATA, 2));
 %analyze individual channels / groups of channels
 N = size(FFTDATA,1);
 [PRDG, w] = periodogram(FFTDATA,rectwin(N),N,60);
+
+%debug
+% N
+% size(w)
+% size(PRDG)
+
 %plot analysis
-figure('NumberTitle', 'off', 'Name', [fName ' FFT analysis']);
     subplot(2,2,1);
-        imagesc(FFTDATA);
+        set(gca, 'ColorOrder', ColorSet, 'NextPlot', 'replacechildren');
+        plot(FFTDATA);
         title('FFTDATA');
-
-    subplot(2,2,2);
-        grid on;
-        hold on; % add any enclosed plots to the same graph
-            title('priodogram = plot(w, 10*log10(PRDG))');
-            plot(w, 10*log10(PRDG));
-        hold off;
-
-    subplot(2,2,4);
-        grid on;
-        hold on; % add any enclosed plots to the same graph
-            title('log / log plot = plot(log(w), log(PRDG))');
-            plot(log(w), log(PRDG));
-            % TODO: why is this different?
-            %loglog(w, PRDG);
-        hold off;
         
     subplot(2,2,3);
         imagesc(log(PRDG));
         title('log(PRDG)');
+
+    subplot(2,2,2);
+        set(gca, 'ColorOrder', ColorSet, 'NextPlot', 'replacechildren');
+        grid on;
+        hold on; % add any enclosed plots to the same graph
+            title('priodogram = plot(w, 10*log10(PRDG))');
+            linPRDG = plot(w, 10*log10(PRDG));
+        hold off;
+
+    subplot(2,2,4);
+        set(gca, 'ColorOrder', ColorSet, 'NextPlot', 'replacechildren');
+        grid on;
+        hold on; % add any enclosed plots to the same graph
+            title('log / log plot; lin regression + y intercept');
+            llPRDG = plot(log(w), log(PRDG));
+            % TODO: why is this different?
+            %loglog(w, PRDG);
+            %set PRDG plot style
+            set(llPRDG, 'LineStyle', ':');
+            %capture X and Y data for linear regression analysis
+            llPXData = cell2mat(get(llPRDG, 'XData'))';
+            llPYData = cell2mat(get(llPRDG, 'YData'))';
+            %set -Inf values to zero
+            llPXData(llPXData <= 0) = 0;
+            
+            %calculate linear regression + yintercept
+            x = llPXData;
+            y = llPYData;
+            format long
+            os = ones(size(x,1), 1);
+            size(x)
+            size(os)
+            X = [os x];
+            b = X\y;
+            
+            %plot linear regression + y intercept as solid lines
+            yCalc2 = X*b;
+            plot(x,yCalc2);
+        hold off;
 %composit those into a single p/f graph for entire dataset
 %    - need to consider different methodologies
 %create a 2D image to represent entire dataset (channels on x, p/f values on y?)
---
+++ varycolor.m
@@ -0,0 +1,44 @@
+function ColorSet=varycolor(NumberOfPlots)
+% VARYCOLOR Produces colors with maximum variation on plots with multiple
+% lines.
+%
+%     VARYCOLOR(X) returns a matrix of dimension X by 3.  The matrix may be
+%     used in conjunction with the plot command option 'color' to vary the
+%     color of lines.  
+%
+%     Yellow and White colors were not used because of their poor
+%     translation to presentations.
+% 
+%     Example Usage:
+%         NumberOfPlots=50;
+%
+%         ColorSet=varycolor(NumberOfPlots);
+% 
+%         figure
+%         hold on;
+% 
+%         for m=1:NumberOfPlots
+%             plot(ones(20,1)*m,'Color',ColorSet(m,:))
+%         end
+
+%Created by Daniel Helmick 8/12/2008
+%Redesigned by david allen 12/02/2017
+
+narginchk(1,1)%correct number of input arguements??
+nargoutchk(0, 1)%correct number of output arguements??
+
+%Take care of the anomolies
+if NumberOfPlots<1
+    ColorSet=[];
+    
+else %default and where this function has an actual advantage
+    %store colors to interploate in a set
+    %TODO: make color set an input argument
+    colors = [1.0 0.0 0.0; 1.0 1.0 0.0; 0.0 1.0 0.0; 0.0 1.0 1.0; 0.0 0.0 1.0; 1.0 0.0 1.0];
+    n = size(colors, 1);
+    colorwheel = vertcat(colors, colors(1,:));
+    %create vector of how far apart colors should be on color wheel
+    spread = n / NumberOfPlots;
+    spreadS = 1:spread:n;
+    ColorSet = interp1(colorwheel,spreadS);
+end
