2ce1472928689e9a8c8646b3c0c9cbf038c14198
david allen <d@preform.io>
2017-12-03 13:11:08 -0500

    To do comments for batch file.

--- batch.m
+++ batch.m
@@ -1,3 +1,14 @@
+%TODO:
+%     create a time-stamped directory to save out analysis files
+%     turn filenames into an array
+%     capture figures and data for output to analysis table
+%          time to calculate
+%          dimensions of data (filtered and unfiltered)
+%          figures
+%          self-similarity image
+%          slopes analysis (mean, average, spread, etc.)
+%     turn filenames into an array that can be fed into the function
+%     include sample size and other related arguments so that any data type can be handed in
 
 fractal_analysis('calc_files/africa/africa_down_Char00.calc');
 fractal_analysis('calc_files/africa/africa_left_Char00.calc');
