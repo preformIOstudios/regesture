7db93c928b3f77f0364d2701f09e7aa4a241268d
david allen <d@preform.io>
2018-01-26 18:27:00 -0500

    Notes on a potential way to fix Thiel-Sen plots which are giving seemingly garbage output.

--- linreg.m
+++ linreg.m
@@ -19,8 +19,32 @@ function [yCalc, b] = linreg(x,y, method)
         data = permute(rezip, [2,1,3]);
         data(1) = data(1); %TODO: why is this here?
         [m2, b2] = TheilSen(data);
+        %plims = [min(min(data(:,1,:))) max(max(data(:,1,:)))]';
         b = [m2'; b2'; repmat(zeros(1,size(data,3)),size(X,2)-2,1)];
         yCalc = X*b;
+        %TODO: something about this last line above smells funny
+        % check against example code (below)
+            % n=100;
+            % outN=round(0.2*n);
+            % noise = randn(n,2)*0.1; noise(randi(n*2,[outN 1]))=randn(outN,1)*5;
+            % data = [linspace(0,10,n)' linspace(0,5,n)'] + noise;
+            % [m, b] = TheilSen(data);
+            % plims = [min(data(:,1)) max(data(:,1))]';
+            % figure
+            % plot(data(:,1),data(:,2),'k.',...
+            %     plims,plims*m+b,'-r')
+            % legend('Data','TheilSen','Least Squares','location','NW')
+            % title(sprintf('Acual Slope = %2.3g, LS est = %2.3g, TS est = %2.3g',[0.5 Bhat(2) m]))
+        % used the following to get a data array that is multidimensional
+            % dataArr=zeros(100,2,2);
+            % dataArr(:,:,1) = data;
+            % dataArr(:,:,2) = [linspace(10,15,n)' linspace(5,0,n)'] + noise;
+            % plims = [min(min(dataArr(:,1,:))) max(max(data(:,1,:)))]';
+            % plot(squeeze(dataArr(:,1,:)), squeeze(dataArr(:,2,:)), '.');
+            % plot(plims,plims*m'+b','-r'); %output looks somewhat
+            % accurate, but shape of data may be wrong (if it was correct, it would
+            % generate different plot line colors)
+           
     else
         %TODO: thow an error for unknown linear regression
         %type
